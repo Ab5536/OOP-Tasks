7359c2d3f633e907634a57a754de54c5862451c9
Ab5536 <120156737+Ab5536@users.noreply.github.com>
2023-07-05 20:33:14 +0500

    Add files via upload

---
+++ Ocean Navigation Task  Roll no 3/Angle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ocean_Navigation_Task__Roll_no_3
+{
+    class Angle
+    {
+        public int Degrees;
+        public float Minutes;
+        public char Direction;
+
+        public Angle(int Degrees, float Minutes, char Direction)
+        {
+            this.Degrees = Degrees;
+            this.Minutes = Minutes;
+            this.Direction = Direction;
+        }
+    }
+}
